635ab02d2f6c8bebb6800febd60a3c5e04dea745
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-03-24 20:24:39 -0400

    Add a TODO to translation

--- src/Symmetry/translation.jl
+++ src/Symmetry/translation.jl
@@ -67,6 +67,7 @@ function get_generators(hypercube ::HypercubicLattice)
   translations = [translation_element(hypercube, r) for r in hypercube.coordinates]
   group_order = length(translations)
   sorted_indices = sortperm(1:group_order; by=(i->translations[i].order), rev=true, alg=Base.Sort.DEFAULT_STABLE)
+
   function factorize(generator_indices ::AbstractVector{<:Integer}, # Return value if successful
                      translation_groups ::AbstractVector{<:AbstractSet{Permutation}},
                      size_product ::Integer,
@@ -92,7 +93,10 @@ function get_generators(hypercube ::HypercubicLattice)
   generator_indices = Int[]
   sizehint!(generator_indices, group_order) # upper limit
   initial_translation_groups = [generate_group(g) for g in translations]
-  return factorize(generator_indices, initial_translation_groups, 1, 1)
+  generators = factorize(generator_indices, initial_translation_groups, 1, 1)
+  # TODO: add remaining order-1 translations
+
+  return generators
 end
 
 
