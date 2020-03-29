00cdc1c330d49c3ed68521ac1eb061ce4961165a
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-03-28 23:23:17 -0400

    Rename Supercell to lattice

--- src/Basic/lattice.jl
+++ src/Basic/lattice.jl
@@ -1,7 +1,10 @@
 export SuperCell
 export make_supercell
+export make_lattice
+export Lattice
 
 
+#=
 struct SuperCell{O}
     unitcell::UnitCell{O}
     hypercube::HypercubicLattice
@@ -31,6 +34,38 @@ function make_supercell(unitcell ::UnitCell{O}, hypercube::HypercubicLattice) wh
   end
 
   return SuperCell{O}(unitcell, hypercube, new_unitcell)
+end
+=#
+
+
+struct Lattice{O}
+  unitcell::UnitCell{O}
+  hypercube::HypercubicLattice
+  supercell::UnitCell{Tuple{O, Vector{Int}}}
+end
+
+function make_lattice(unitcell::UnitCell{O}, scale_matrix::AbstractMatrix{<:Integer}) where O
+  hypercube = HypercubicLattice(scale_matrix)
+  return make_lattice(unitcell, hypercube)
+end
+
+function make_lattice(unitcell::UnitCell{O}, hypercube::HypercubicLattice) where O
+  hypercube = orthogonalize(hypercube)
+
+  new_latticevectors = unitcell.latticevectors * hypercube.scale_matrix
+  inverse_scale_matrix = hypercube.inverse_scale_matrix
+  unitcell_coordinates = hypercube.coordinates
+
+  new_unitcell = make_unitcell(new_latticevectors; OrbitalType=Tuple{O, Vector{Int}})
+  for uc in unitcell_coordinates, (orbname, orbcoord) in unitcell.orbitals
+    cc = fract2carte(unitcell, orbcoord)
+    new_cc = cc + unitcell.latticevectors * uc
+    new_orbcoord = carte2fract(new_unitcell, new_cc)
+    new_orbname = (orbname, uc)
+    addorbital!(new_unitcell, new_orbname, new_orbcoord)
+  end
+
+  return Lattice{O}(unitcell, hypercube, new_unitcell)
 end
 
 
