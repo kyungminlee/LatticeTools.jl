3bc652d9a3eb1b81806cd22727c8f76a4b6fc1a0
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-03-29 12:19:12 -0400

    Add test_basic

--- test/test_basic.jl
+++ test/test_basic.jl
@@ -11,4 +11,12 @@ using TightBindingLattice
     @test 1.5im == parse_expr("1.5i")
     @test [1,2,3] == parse_expr("[1,2,3]")
     @test [1,2,3im] == parse_expr([1, "2", "3i"])
+
+
+    using TightBindingLattice: cleanup_number
+
+    @test cleanup_number(42, 1E-8) == 42
+    @test cleanup_number(1.5 + 1E-12, 1E-8) == 1.5
+    @test cleanup_number([1.0 + 1E-12, 2.0 - 1E-12, 3.0], 1E-8) == [1.0, 2.0, 3.0]
+    @test cleanup_number(0.1234567, 1E-8) == 0.1234567
 end
