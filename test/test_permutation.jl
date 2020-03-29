3f3560d084fb725f02d022c3fea9d7adc8556156
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-03-29 01:28:13 -0400

    Update test_permutation

--- test/test_permutation.jl
+++ test/test_permutation.jl
@@ -21,6 +21,22 @@ using TightBindingLattice
   @test p2.order == 2
   @test p3.order == 4
 
+  @test inverse(p0) == p0
+  @test inverse(p1) == p3
+  @test inverse(p2) == p2
+  @test inverse(p3) == p1
+
+  @test isequal(p0, Permutation([1,2,3,4]))
+  @test isless(p0, p1)
+  @test isless(p1, p3)
+
   @test_throws ArgumentError Permutation([1,2,3,4]) * Permutation([1,2,3,4,5])
   @test hash(Permutation(Int[1,2,3,4])) == hash(Int[1,2,3,4])
+
+
+  @test generate_group(p1) == Set([p0, p1, p2, p3])
+  @test generate_group(p2) == Set([p0, p2])
+  @test generate_group(p1, p2) == Set([p0, p1, p2, p3])
+
+
 end
