377c402d8db4af167c781b34b3567b2838f8ee78
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-03-29 01:16:31 -0400

    Add test for lattice

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,6 @@ include("test_basic.jl")
 include("test_coord.jl")
 include("test_unitcell.jl")
 include("test_hypercube.jl")
-
+include("test_lattice.jl")
 include("test_group.jl")
 include("test_symmetry.jl")
