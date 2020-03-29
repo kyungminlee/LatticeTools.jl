4c726d8650d05e478184d39c580e31915b97c574
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-03-28 23:23:47 -0400

    Update TightBindingLattice.jl

--- src/TightBindingLattice.jl
+++ src/TightBindingLattice.jl
@@ -1,21 +1,24 @@
 module TightBindingLattice
 
 include("Basic/basic.jl")
+include("Basic/permutation.jl")
 include("Basic/coord.jl")
 include("Basic/hypercube.jl")
 include("Basic/unitcell.jl")
-include("Basic/permutation.jl")
+include("Basic/lattice.jl")
 
 include("Group/abstractgroup.jl")
 include("Group/finitegroup.jl")
-include("Group/finiteabeliangroup.jl")
+#include("Group/finiteabeliangroup.jl")
 
 include("Symmetry/abstractsymmetry.jl")
-include("Symmetry/translation.jl")
-include("Symmetry/point.jl")
+include("Symmetry/translationsymmetry.jl")
+include("Symmetry/pointsymmetry.jl")
 
-include("enlargement.jl")
+export PointSymmetryDatabase
+include("Symmetry/pointsymmetrydatabase.jl")
+
+#include("enlargement.jl")
 include("momentumpath.jl")
-
 
 end # module
