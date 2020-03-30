3d22dfacbba2bf326a397525416b03e89598e7b5
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-03-29 20:59:10 -0400

    Delete display.jl

--- src/display.jl
+++
@@ -1,9 +0,0 @@
-function display(unitcell ::UnitCell{O}) where O
-  lvs = unitcell.latticevectors
-  arrow(0,0, lvs[:,1]...)
-  arrow(0,0, lvs[:,2]...)
-
-  orbital_ccs = transpose(hcat([fract2carte(unitcell, orbcoord) for (orbname, orbcoord) in unitcell.orbitals]...))
-  plot(orbital_ccs[:,1], orbital_ccs[:,2], "o")
-end
-
