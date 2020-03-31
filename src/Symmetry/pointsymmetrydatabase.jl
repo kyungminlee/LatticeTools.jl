e5cfdbf178e1796713f14cf2cbc96a9f965af065
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-03-31 12:47:47 -0400

    Update pointsymmetrydatabase (cache database with JLD2)
    Update psymdb

--- src/Symmetry/pointsymmetrydatabase.jl
+++ src/Symmetry/pointsymmetrydatabase.jl
@@ -1,12 +1,14 @@
 module PointSymmetryDatabase
 using YAML
+using JLD2
+using FileIO
 
 import ..TightBindingLattice: PointSymmetry, read_point_symmetry
 
 NUM_POINT_SYMMETRIES = 32
 POINT_SYMMETRY_DATABASE = Vector{PointSymmetry}(undef, NUM_POINT_SYMMETRIES)
 
-function load(groupnum)
+function load_group(groupnum)
     data_directory = abspath(joinpath(@__DIR__, "..", "..", "data", "PointGroup3D"))
     file_path = joinpath(data_directory, "PointGroup3D-$groupnum.yaml")
     data_yaml = YAML.load_file(file_path)
@@ -14,20 +16,21 @@ function load(groupnum)
     return point_symmetry
 end
 
-#=
-function get(name::String)
-    idx = find(x -> x.hermann_mauguinn == name, POINT_SYMMETRY_DATABASE)
-    isnothing(idx) && throw(ArgumentError("group with name \"$name\" not found"))
-    return get(idx)
-end
-=#
-
 function get(groupnum::Integer)
     (groupnum < 1 || groupnum > NUM_POINT_SYMMETRIES) && throw(ArgumentError("group #$groupnum not found"))
     if !isassigned(POINT_SYMMETRY_DATABASE, groupnum)
-        POINT_SYMMETRY_DATABASE[groupnum] = load(groupnum)
+        POINT_SYMMETRY_DATABASE[groupnum] = load_group(groupnum)
     end
     return POINT_SYMMETRY_DATABASE[groupnum]
+end
+
+let data_directory = abspath(joinpath(@__DIR__, "..", "..", "data", "PointGroup3D"))
+    if isfile(joinpath(data_directory, "PointGroup3D.jld2"))
+        cache_database = load(joinpath(data_directory, "PointGroup3D.jld2"), "POINT_SYMMETRY_DATABASE")
+        for gn in 1:NUM_POINT_SYMMETRIES
+            POINT_SYMMETRY_DATABASE[gn] = cache_database[gn]
+        end
+    end
 end
 
 end # module PointSymmetryDatabase
