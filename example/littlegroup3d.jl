444dda024e53e2b1b19fe242ea9810d698353695
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-03-31 12:46:01 -0400

    Add example/littlegroup3d.jl

---
+++ example/littlegroup3d.jl
@@ -0,0 +1,31 @@
+using TightBindingLattice
+
+# Little group
+
+unitcell = make_unitcell([1.0 0.0 0.0; 0.0 1.0 0.0; 0 0 1]; OrbitalType=String)
+addorbital!(unitcell, "Ox", FractCoord([0,0,0], [0.5, 0.0, 0.0]))
+addorbital!(unitcell, "Oy", FractCoord([0,0,0], [0.0, 0.5, 0.0]))
+
+psym = project(PointSymmetryDatabase.get(15), [1 0 0; 0 1 0; 0 0 1])
+#idx_C4 = 3
+#@show psym
+#@show typeof(psym)
+
+lattice = make_lattice(unitcell, [4 0 0; 0 4 0; 0 0 3])
+perms = get_orbital_permutations(lattice, psym)
+
+tsym = TranslationSymmetry(lattice)
+
+@show num_irreps(tsym)
+
+@show tsym.orthogonal_coordinates
+
+
+for idx in 1:num_irreps(tsym)
+    #@show little_symmetry(tsym, idx, psym)
+    psym_little = little_symmetry(tsym, idx, psym)
+    @show idx
+    @show tsym.hypercube.coordinates[idx]
+    @show iscompatible(tsym, idx, psym)
+    @show iscompatible(tsym, idx, psym_little)
+end
