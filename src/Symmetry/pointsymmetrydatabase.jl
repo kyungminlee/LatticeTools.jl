248e0577e228004d1256bed10cc88f457a6449a3
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-04-06 14:55:24 -0400

    Update pointsymmetrydatabase

--- src/Symmetry/pointsymmetrydatabase.jl
+++ src/Symmetry/pointsymmetrydatabase.jl
@@ -1,13 +1,15 @@
+
 module PointSymmetryDatabase
+
 using YAML
 using JLD2
 using FileIO
 
-import ..TightBindingLattice: PointSymmetry, read_point_symmetry
+import ..TightBindingLattice: PointSymmetry, read_point_symmetry, simplify_name
 
 NUM_POINT_SYMMETRIES = 32
 POINT_SYMMETRY_DATABASE = Vector{PointSymmetry}(undef, NUM_POINT_SYMMETRIES)
-
+POINT_SYMMETRY_LOOKUP = Dict{Vector{String}, Int}()
 
 function __init__()
     data_directory = abspath(joinpath(@__DIR__, "..", "..", "data", "PointGroup3D"))
@@ -16,6 +18,11 @@ function __init__()
         for gn in 1:NUM_POINT_SYMMETRIES
             POINT_SYMMETRY_DATABASE[gn] = cache_database[gn]
         end
+    end
+	# println("$(@__MODULE__)::__init")
+    for i in 1:NUM_POINT_SYMMETRIES
+        psym = get(i)
+        POINT_SYMMETRY_LOOKUP[sort(simplify_name.(psym.element_names))] = i
     end
 end
 
@@ -33,6 +40,11 @@ function get(groupnum::Integer)
         POINT_SYMMETRY_DATABASE[groupnum] = load_group(groupnum)
     end
     return POINT_SYMMETRY_DATABASE[groupnum]
+end
+
+function find(element_names::AbstractVector{<:AbstractString})
+    simple_names = sort(simplify_name.(element_names))
+    return POINT_SYMMETRY_LOOKUP[simple_names]
 end
 
 end # module PointSymmetryDatabase
