dfcb60ae231a6a990b2813fd49ce7010d8a0da88
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-04-07 21:41:59 -0400

    Update doctest for PERMUTATION!!

--- src/Basic/permutation.jl
+++ src/Basic/permutation.jl
@@ -65,10 +65,10 @@ BUT THIS: Return `[p1.map[x] for x in p2.map]`.
 ```jldoctest
 julia> using TightBindingLattice
 
-julia> Permutation([1,3,2]) * Permutation([2,1,3])
+julia> Permutation([2,1,3]) * Permutation([1,3,2])
 Permutation([2, 3, 1], 3)
 
-julia> Permutation([2,1,3]) * Permutation([1,3,2])
+julia> Permutation([1,3,2]) * Permutation([2,1,3])
 Permutation([3, 1, 2], 3)
 ```
 """
