01d528db287d3933786d2d075c1747f5ea3c31da
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-04-12 15:45:25 -0400

    Rename spacesymmetry.jl to symmetryembedding.jl

--- src/Symmetry/symmetryembedding.jl
+++ src/Symmetry/symmetryembedding.jl
@@ -3,20 +3,25 @@
 struct TranslationSymmetryEmbedding
     lattice::Lattice
     symmetry::TranslationSymmetry
+    operations::Vector{Permutation}
+
     function TranslationSymmetryEmbedding(lattice::Lattice, symmetry::TranslationSymmetry)
-        new(lattice, symmetry)
+        permutations = get_orbital_permutations(lattice, symmetry)
+        new(lattice, symmetry, permutations)
     end
 end
 
 struct PointSymmetryEmbedding
     lattice::Lattice
     symmetry::PointSymmetry
+    operations::Vector{Permutation}
 
     function PointSymmetryEmbedding(lattice::Lattice, symmetry::PointSymmetry)
         if !iscompatible(lattice.hypercube, symmetry)
             throw(ArgumentError("lattice and point symmetry not compatible"))
         end
-        new(lattice, symmetry)
+        permutations = get_orbital_permutations(lattice, symmetry)
+        new(lattice, symmetry, permutations)
     end
 end
 
@@ -24,6 +29,7 @@ struct SymmorphicSpaceSymmetryEmbedding
     lattice::Lattice
     translation_symmetry::TranslationSymmetry
     point_symmetry::PointSymmetry
+    operations::Matrix{Permutation}
 
     function SymmorphicSpaceSymmetryEmbedding(
                 lattice::Lattice,
@@ -32,6 +38,10 @@ struct SymmorphicSpaceSymmetryEmbedding
         if !iscompatible(translation, point)
             throw(ArgumentError("translation symmetry and point symmetry not compatible"))
         end
-        new(lattice, translation, point)
+        tperms = get_orbital_permutations(lattice, translation)
+        pperms = get_orbital_permutations(lattice, point)
+        permutations = [pop * top for top in tperms, pop in pperms]
+        @assert size(permutations) == (length(tperms), length(pperms))
+        new(lattice, translation, point, permutations)
     end
 end
