078e0421ccab975ec2f043e0e066d7db2b6febd4
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-04-13 12:25:27 -0400

    Add tests for IdentityOperation

--- test/test_symmetryoperation.jl
+++ test/test_symmetryoperation.jl
@@ -4,14 +4,26 @@ using TightBindingLattice
 @testset "symmetryoperations" begin
 
     latticevectors = [1.0 -0.5; 0.0 sqrt(3.0)*0.5]
+    iden = IdentityOperation()
+
+    @testset "identity" begin
+        @test inverse(iden) == iden
+        @test apply_symmetry(iden, [3,2,1]) == [3,2,1]
+        @test iden([3,2,1]) == [3,2,1]
+        @test canonize(iden) == iden
+        @test iscanonical(iden)
+    end
 
     @testset "translation" begin
         t1 = TranslationOperation([1,0])
         t2 = TranslationOperation([2,4])
-
+        
         @test t2 == TranslationOperation{Int}([2,4])
         @test dimension(t2) == 2
         @test t2^2 == TranslationOperation([4,8])
+
+        @test t1 * iden == t1
+        @test iden * t1 == t1
 
         t3 = t1*t2
         @test t3.displacement == [3,4]
