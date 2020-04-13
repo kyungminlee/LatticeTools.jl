c908dfafbd86c31c3ad5e258f1ee3c7c20c072ea
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-04-13 16:44:00 -0400

    Update spaceoperation (WIP)
    Not used yet.

--- src/SymmetryOperation/spaceoperation.jl
+++ src/SymmetryOperation/spaceoperation.jl
@@ -1,8 +1,9 @@
 export SpaceOperation
-
 export apply_operation
 export canonize
 export iscanonical
+
+using LinearAlgebra
 
 struct SpaceOperation{S<:Real}
     matrix::Matrix{S}
@@ -10,6 +11,12 @@ struct SpaceOperation{S<:Real}
 
     function SpaceOperation(matrix::AbstractMatrix{S},
                             displacement::AbstractVector{S}) where {S<:Real}
+        dim = size(matrix, 1)
+        if size(matrix, 2) != dim
+            throw(DimensionMismatch("matrix is not square: dimensions are $(size(matrix))"))
+        elseif length(displacement) != dim
+            throw(DimensionMismatch("dimensions of matrix and displacement do not match: matrix dimensions are $(size(matrix)), and vector dimension is $(length(displacement))"))
+        end
         return new{S}(matrix, displacement)
     end
 
@@ -17,18 +24,25 @@ struct SpaceOperation{S<:Real}
                             translation::TranslationOperation{S}) where {S<:Real}
         return new{S}(point.matrix, translation.displacement)
     end
-end
 
-
-import Base.*
-function (*)(lhs::ProductOperation, rhs::TranslationOperation)
-    SpaceOperation(lhs.matrix, rhs.displacement)
-end
-
-
-function (*)(lhs::TranslationOperation, rhs::ProductOperation)
-    rhs_inv = inv(rhs)
-    SpaceOperation(rhs, TranslationOperation(rhs_inv.matrix * lhs.displacement))
+    function SpaceOperation(arg::ProductOperation{S, F}) where {S, F}
+        dim = dimension(arg)
+        if dim == 0
+            throw(ArgumentError("Cannot create space operation out of an identity (dimension undetermined)"))
+        end
+        
+        canon = canonize(arg)
+        if isa(canon, IdentityOperation)
+            return SpaceOperation(Matrix(I, dim, dim), zeros(S, dim))
+        elseif isa(canon, TranslationOperation)
+            return SpaceOperation(Matrix(I, dim, dim), canon.displacement)
+        elseif isa(canon, PointOperation)
+            return SpaceOperation(canon.matrix, zeros(S, dim))
+        else
+            @assert length(canon.factors) == 2
+            return SpaceOperation(canon.factors[1], canon.factors[2])
+        end
+    end
 end
 
 
