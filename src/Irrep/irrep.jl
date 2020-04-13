4a7138f4f0dbe90e2f53952fb7d972f58f25d634
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-04-13 19:09:12 -0400

    Update irreps according to removal of get_orbital_permutations

--- src/Irrep/irrep.jl
+++ src/Irrep/irrep.jl
@@ -1,17 +1,13 @@
 export IrrepData
+
 export AbstractSymmetryIrrepComponent
 export IrrepComponent
 export SymmorphicIrrepComponent
-# export TranslationSymmetryIrrepComponent
-# export PointSymmetryIrrepComponent
-# export SymmorphicSpaceSymmetryIrrepComponent
-
 
 export group_order
 export get_irrep_components
 export get_irrep_iterator
 export little_symmetry
-
 
 struct IrrepData
     group::FiniteGroup
@@ -20,9 +16,7 @@ struct IrrepData
     irreps::Vector{Vector{Matrix{ComplexF64}}}
 end
 
-
 abstract type AbstractSymmetryIrrepComponent end
-
 
 struct IrrepComponent{SymmetryType}<:AbstractSymmetryIrrepComponent
     symmetry::SymmetryType
@@ -68,9 +62,6 @@ function get_irrep_iterator(sic::IrrepComponent)
 end
 
 
-
-
-
 struct SymmorphicIrrepComponent{S1<:AbstractSymmetry, S2<:AbstractSymmetry} <:AbstractSymmetryIrrepComponent
 
     component1::IrrepComponent{S1} # e.g. Translation
@@ -92,29 +83,43 @@ struct SymmorphicIrrepComponent{S1<:AbstractSymmetry, S2<:AbstractSymmetry} <:Ab
     end
 end
 
+
 group_order(sic::SymmorphicIrrepComponent) = group_order(sic.component1) * group_order(sic.component2)
 
 
 function little_group_elements(tsic::IrrepComponent{TranslationSymmetry},
-                               psym::PointSymmetry)
+                               psym::PointSymmetry) ::Vector{Int}
     return little_group_elements(tsic.symmetry, tsic.irrep_index, psym)
 end
 
+function little_group_elements(tsic::IrrepComponent{SymmetryEmbedding{TranslationSymmetry}},
+                               psymbed::SymmetryEmbedding{PointSymmetry}) ::Vector{Int}
+    return little_group_elements(symmetry(tsic.symmetry), tsic.irrep_index, symmetry(psym))
+end
 
 function little_group(tsic::IrrepComponent{TranslationSymmetry},
-                      psym::PointSymmetry)
+                      psym::PointSymmetry)::FiniteGroup
     return little_group(tsic.symmetry, tsic.irrep_index, psym)
 end
 
-# function little_group(tsic::IrrepComponent{TranslationSymmetryEmbedding},
-#                       psym::PointSymmetry)
-#     return little_group(tsic.symmetry.symmetry, tsic.irrep_index, psym)
-# end
+function little_group(tsic::IrrepComponent{SymmetryEmbedding{TranslationSymmetry}},
+                      psymbed::SymmetryEmbedding{PointSymmetry})::FiniteGroup
+    return little_group(symmetry(tsic.symmetry), tsic.irrep_index, symmetry(psymbed))
+end
 
 
 function little_symmetry(tsic::IrrepComponent{TranslationSymmetry},
-                         psym::PointSymmetry)
+                         psym::PointSymmetry)::PointSymmetry
     return little_symmetry(tsic.symmetry, tsic.irrep_index, psym)
+end
+
+
+function little_symmetry(tsic::IrrepComponent{SymmetryEmbedding{TranslationSymmetry}},
+                         psymbed::SymmetryEmbedding{PointSymmetry})::SymmetryEmbedding{PointSymmetry}
+    psym_little = little_symmetry(symmetry(tsic.symmetry),
+                                  tsic.irrep_index,
+                                  symmetry(psymbed.symmetry))
+    return embed(psymbed.lattice, psym_little)
 end
 
 
@@ -125,8 +130,17 @@ end
 
 
 function iscompatible(tsic::IrrepComponent{TranslationSymmetry},
-                      psym::PointSymmetry)
+                      psym::PointSymmetry)::Bool
     return iscompatible(tsic.symmetry, tsic.irrep_index, psym)
+end
+
+
+function iscompatible(tsic::IrrepComponent{SymmetryEmbedding{TranslationSymmetry}},
+                      psymbed::SymmetryEmbedding{PointSymmetry})::Bool
+    # TODO: Check lattice?
+    return iscompatible(symmetry(tsic.symmetry),
+                        tsic.irrep_index,
+                        symmetry(psymbed))
 end
 
 # function iscompatible(tsic::IrrepComponent{TranslationSymmetryEmbedding},
@@ -134,8 +148,9 @@ end
 #     return iscompatible(tsic.symmetry.symmetry, tsic.irrep_index, psym)
 # end
 
-
-function get_irrep_components(tsym::S1, psym::S2) where {S1<:AbstractSymmetry, S2<:AbstractSymmetry}
+function get_irrep_components(tsym::S1,
+                              psym::S2) where {S1<:AbstractSymmetry,
+                                               S2<:AbstractSymmetry}
     return (SymmorphicIrrepComponent(tsic, psic)
                 for tsic in get_irrep_components(tsym)
                 for psic in get_irrep_components(little_symmetry(tsic, psym)))
@@ -152,23 +167,18 @@ function get_irrep_iterator(lattice::Lattice,
     psym = ssic.component2.symmetry
     psym_irrep_index = ssic.component2.irrep_index
     psym_irrep_compo = ssic.component2.irrep_component
-
-    tsym_permutations = get_orbital_permutations(lattice, tsym)
+    
+    tsym_elements = elements(tsym)
     tsym_irrep = irrep(tsym, tsym_irrep_index)
     tsym_irrep_components = [m[tsym_irrep_compo, tsym_irrep_compo] for m in tsym_irrep]
 
-    psym_permutations = get_orbital_permutations(lattice, psym)
+    psym_elements = elements(psym)
     psym_irrep = irrep(psym, psym_irrep_index)
     psym_irrep_components = [m[psym_irrep_compo, psym_irrep_compo] for m in psym_irrep]
 
-    return (
-        (psym_perm * tsym_perm ,  psym_phase * tsym_phase)
-        for (psym_perm, psym_phase) in zip(psym_permutations,
-                                           psym_irrep_components)
-        for (tsym_perm, tsym_phase) in zip(tsym_permutations,
-                                           tsym_irrep_components)
-        #if !isapprox(psym_phase, 0; atol=tol) && !isapprox(psym_phase, 0; atol=tol)
-    )
+    return ((psym_elem * tsym_elem, psym_phase * tsym_phase)
+                for (psym_elem, psym_phase) in zip(psym_elements, psym_irrep_components)
+                for (tsym_elem, tsym_phase) in zip(tsym_elements, tsym_irrep_components))
 end
 
 
