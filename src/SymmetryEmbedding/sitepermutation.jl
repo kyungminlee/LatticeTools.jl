73c73ecd5e9fa9e6e00532a240ad2fb0179fa65a
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-04-13 21:25:19 -0400

    Update sitepermutation.jl

--- src/SymmetryEmbedding/sitepermutation.jl
+++ src/SymmetryEmbedding/sitepermutation.jl
@@ -55,7 +55,7 @@ end
 Embed the simplest version of point operation. (no local unitary operation)
 """
 function embed(lattice::Lattice, pop::PointOperation)
-    orbital_map = findorbitalmap(lattice.unitcell, pop.matrix)
+    orbital_map = findorbitalmap(lattice.unitcell, pop)
     if isnothing(orbital_map)
         throw(ArgumentError("lattice not compatible with $pop"))
     end
