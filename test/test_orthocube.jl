5e0ab2c187f3286c07c54ac13b53ee640ca8a69c
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-04-19 00:12:36 -0400

    Update test_orthocube.jl

--- test/test_orthocube.jl
+++ test/test_orthocube.jl
@@ -10,6 +10,9 @@ using TightBindingLattice
         orthocube = OrthoCube([3 0; 0 3])
         @test dimension(orthocube) == 2
         @test isequiv(orthocube, OrthoCube([3 0; 3 3]))
+        @test !isequiv(orthocube, OrthoCube([3 0; 1 3]))
+        @test !isequiv(orthocube, OrthoCube([6 0; 0 6]))
+        @test !isequiv(OrthoCube([6 0; 0 6]), orthocube)
 
         for i in 0:6, j in 0:6
             @test orthocube.wrap([i, j]) == ([i÷3, j÷3], [i%3, j%3])
@@ -29,6 +32,16 @@ using TightBindingLattice
         @test orthocube.wrap([1,0]) == ([0, -1], [2, 2])
         @test orthocube.wrap([2,0]) == ([1, -1], [1, 1])
         @test orthocube.wrap([3,0]) == ([2, -1], [0, 0])
+    end
+
+    @testset "generator" begin
+        ortho = OrthoCube([4 0; 0 3])
+        generate_coordinates(ortho, [ 1 4; -1 -3])
+        generate_coordinates(ortho, [ 1 0; 0 1])
+        @test_throws DimensionMismatch generate_coordinates(ortho, [ 1 0 0; 0 1 0]) # not square
+        @test_throws ArgumentError generate_coordinates(ortho, [ 2 0; 0 1]) # unimodular
+        @test_throws ArgumentError generate_coordinates(ortho, [ 1 0; 1 1])
+        # c2 = generate_coordinates(ortho, [1 0; 0 1])
     end
 
 end
