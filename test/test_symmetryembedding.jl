7a78b81bc703cf843970e9a19d78040c999abe6a
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-04-19 17:13:59 -0400

    Update test_symmetryembedding with symmorphic versions

--- test/test_symmetryembedding.jl
+++ test/test_symmetryembedding.jl
@@ -114,18 +114,21 @@ using LinearAlgebra
         end
         tsym_embed = embed(kagome.lattice, kagome.translation_symmetry)
         @test_throws ArgumentError embed(kagome.lattice, kagome.point_symmetry)
-        @test_throws ArgumentError embed(kagome.lattice, kagome.translation_symmetry, kagome.point_symmetry)
+        # @test_throws ArgumentError embed(kagome.lattice, kagome.translation_symmetry, kagome.point_symmetry)
+        @test_throws ArgumentError embed(kagome.lattice, kagome.translation_symmetry ⋊ kagome.point_symmetry)
 
         # lattice too small for faithful embedding
         kagome = make_kagome_lattice([1 0; 0 1])
         tsym_embed = embed(kagome.lattice, kagome.translation_symmetry)
         @test_throws ArgumentError embed(kagome.lattice, kagome.point_symmetry)
-        @test_throws ArgumentError embed(kagome.lattice, kagome.translation_symmetry, kagome.point_symmetry)
+        # @test_throws ArgumentError embed(kagome.lattice, kagome.translation_symmetry, kagome.point_symmetry)
+        @test_throws ArgumentError embed(kagome.lattice, kagome.translation_symmetry ⋊ kagome.point_symmetry)
 
         kagome = make_kagome_lattice([2 0; 0 2])
         tsym_embed = embed(kagome.lattice, kagome.translation_symmetry)
         psym_embed = embed(kagome.lattice, kagome.point_symmetry)
-        ssym_embed = embed(kagome.lattice, kagome.translation_symmetry, kagome.point_symmetry)
+        # ssym_embed = embed(kagome.lattice, kagome.translation_symmetry, kagome.point_symmetry)
+        ssym_embed = embed(kagome.lattice, kagome.translation_symmetry ⋊ kagome.point_symmetry)
 
         @test group_order(tsym_embed) == group_order(kagome.translation_symmetry)
         @test group_order(psym_embed) == group_order(kagome.point_symmetry)
@@ -134,7 +137,8 @@ using LinearAlgebra
         kagome = make_kagome_lattice([4 -2; 2 2])
         tsym_embed = embed(kagome.lattice, kagome.translation_symmetry)
         psym_embed = embed(kagome.lattice, kagome.point_symmetry)
-        ssym_embed = embed(kagome.lattice, kagome.translation_symmetry, kagome.point_symmetry)
+        # ssym_embed = embed(kagome.lattice, kagome.translation_symmetry, kagome.point_symmetry)
+        ssym_embed = embed(kagome.lattice, kagome.translation_symmetry ⋊ kagome.point_symmetry)
     end
 
 end
