746f9f910b0f7a20a7dced4414e7de7788ef7f0a
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-04-21 18:59:25 -0400

    Update test_lattice.jl

--- test/test_lattice.jl
+++ test/test_lattice.jl
@@ -25,17 +25,28 @@ end
 
     @test_throws DimensionMismatch make_lattice(unitcell, 3)
     @test_throws DimensionMismatch make_lattice(unitcell, [1 0 0; 0 1 0; 0 0 1])
+    @test_throws ArgumentError make_lattice(unitcell, [4 0; 0 3], [1 1; 0 1]) # generators not orthogonal
+    @test_throws ArgumentError make_lattice(unitcell, [4 0; 0 4], [1 0; 0 2]) # generator not unimodular
 
     lattice = make_lattice(unitcell, [2 0; 0 2])
 
     @test lattice.unitcell == unitcell
-    # @test lattice.hypercube.scale_matrix == [2 0; 0 2]
     @test lattice.orthocube.shape_matrix == [2 0; 0 2]
     @test numorbital(lattice.supercell) == 3 * 2 * 2
     @test lattice.supercell.latticevectors == [2.0 0.0; 0.0 2.0]
 
     @test lattice == make_lattice(unitcell, [2 0; 0 2])
     @test lattice != make_lattice(unitcell, [3 0; 0 2])
+
+    let lattice2 = make_lattice(unitcell, [2 0; 0 2], [1 0; 0 1])
+        @test lattice == lattice2
+    end
+    let lattice3 = make_lattice(unitcell, [2 0; 0 2], [0 1; 1 0])
+        @test lattice != lattice3
+        @test lattice.unitcell == lattice3.unitcell
+        @test lattice.orthocube == lattice3.orthocube
+        @test lattice.bravais_coordinates != lattice3.bravais_coordinates
+    end
     let unitcell2 = make_unitcell([1.0 0.0; 0.0 1.0]; OrbitalType=String)
         addorbital!(unitcell2, "d", FractCoord([0,0], [0.0, 0.0]))
         @test lattice != make_lattice(unitcell2, [2 0; 0 2])
