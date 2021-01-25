cdb913851c2a31d82f4e990e4e1dfb333236604c
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2021-01-25 11:40:42 -0500

    Add orbitals to unitcell

--- src/Basic/unitcell.jl
+++ src/Basic/unitcell.jl
@@ -10,6 +10,15 @@ export numsite, sitecount,
        getsitecoord,
        getsiteindexcoord,
        getsitename
+       
+export numorbital, numorbitals, orbitalcount
+export addorbital!
+export hasorbital
+export getorbitalindex
+export getorbital
+export getorbitalname
+export getorbitalsiteindex
+export getorbitalcoord
 
 export carte2fract,
        fract2carte,
@@ -28,6 +37,9 @@ import LinearAlgebra
 #        getorbitalcoord,
 #        getorbitalindexcoord,
 #        getorbitalname
+
+
+
 
 # @deprecate numorbital(args...) numsite(args...)
 # @deprecate hasorbital(args...) hassite(args...)
@@ -59,30 +71,35 @@ rather than the constructor to make a UnitCell object.
 - `sites::Vector{Tuple{T, FractCoord}}`: List of sites within unit cell
 - `siteindices::Dict{T, Int}`: Indices of sites
 """
-mutable struct UnitCell{S}
-    latticevectors::Array{Float64, 2}
+mutable struct UnitCell{S, O}
+    latticevectors::Matrix{Float64}
     sites::Vector{Tuple{S, FractCoord}}
+    orbitals::Vector{Tuple{O, Int}}
 
-    reducedreciprocallatticevectors::Array{Float64, 2}
-    reciprocallatticevectors::Array{Float64, 2}
+    reducedreciprocallatticevectors::Matrix{Float64}
+    reciprocallatticevectors::Matrix{Float64}
     siteindices::Dict{S, Int}
+    orbitalindices::Dict{O, Int}
 
-    function UnitCell{S}(
-        latticevectors::AbstractArray{<:Real, 2},
+    function UnitCell{S,O}(
+        latticevectors::AbstractMatrix{<:Real},
         sites::AbstractVector{Tuple{S, FractCoord}},
-        reducedreciprocallatticevectors::AbstractArray{<:Real, 2},
-        reciprocallatticevectors::AbstractArray{<:Real, 2},
-        siteindices::AbstractDict{S, Int}
-    ) where {S}
-        if (S <: Integer)
-            throw(ArgumentError("Integer SiteType is disallowed to avoid confusion"))
-        end
-        new{S}(
+        orbitals::AbstractVector{Tuple{O, Int}},
+        reducedreciprocallatticevectors::AbstractMatrix{<:Real},
+        reciprocallatticevectors::AbstractMatrix{<:Real},
+        siteindices::AbstractDict{S, Int},
+        orbitalindices::AbstractDict{O, Int},
+    ) where {S, O}
+        (S <: Integer) && throw(ArgumentError("Integer SiteType is disallowed to avoid confusion"))
+        (O <: Integer) && throw(ArgumentError("Integer OrbitalType is disallowed to avoid confusion"))
+        new{S, O}(
             latticevectors,
             sites,
+            orbitals,
             reducedreciprocallatticevectors,
             reciprocallatticevectors,
             siteindices,
+            orbitalindices,
         )
     end
 end
@@ -104,6 +121,7 @@ Construct an n-dimensional lattice.
 function makeunitcell(
     latticevectors::AbstractArray{<:Real, 2};
     SiteType::DataType=Any,
+    OrbitalType::DataType=Any,
     tol::Real=Base.rtoldefault(Float64)
 )
     (ndim, ndim_) = size(latticevectors)
@@ -118,23 +136,30 @@ function makeunitcell(
     reduced_rlv = transpose(inv(latticevectors))
     sites = Tuple{SiteType, FractCoord}[]
     siteindices = Dict{SiteType, Int}()
-    return UnitCell{SiteType}(latticevectors, sites, reduced_rlv, 2*π*reduced_rlv, siteindices)
+    orbitals = Tuple{OrbitalType, Int}[]
+    orbitalindices = Dict{OrbitalType, Int}()
+    return UnitCell{SiteType, OrbitalType}(
+        latticevectors, sites, orbitals,
+        reduced_rlv, 2*π*reduced_rlv, siteindices, orbitalindices
+    )
 end
 
 function makeunitcell(
     latticevectors::AbstractVector{<:AbstractVector};
     SiteType::DataType=Any,
-    tol::Real=Base.rtoldefault(Float64)
+    OrbitalType::DataType=Any,
+    tol::Real=Base.rtoldefault(Float64),
 )
-    return makeunitcell(hcat(latticevectors...); SiteType=SiteType, tol=tol)
+    return makeunitcell(hcat(latticevectors...); SiteType=SiteType, OrbitalType=OrbitalType, tol=tol)
 end
 
 function makeunitcell(
     latticeconstant::Real;
     SiteType::DataType=Any,
+    OrbitalType::DataType=Any,
     tol::Real=Base.rtoldefault(Float64),
 )
-    return makeunitcell(hcat(latticeconstant); SiteType=SiteType, tol=tol)
+    return makeunitcell(hcat(latticeconstant); SiteType=SiteType, OrbitalType=OrbitalType, tol=tol)
 end
 
 
@@ -181,7 +206,7 @@ Add an site to the unit cell.
 * `sitename ::{T}`
 * `sitecoord ::FractCoord`
 """
-function addsite!(uc::UnitCell{O}, sitename::O, sitecoord::FractCoord) where {O}
+function addsite!(uc::UnitCell{S, O}, sitename::S, sitecoord::FractCoord) where {S, O}
     (ndim, ndim_) = size(uc.latticevectors)
     if dimension(sitecoord) != ndim
         throw(ArgumentError("sitecoord has wrong dimension"))
@@ -211,7 +236,7 @@ Test whether the unit cell contains the site of given name.
 * `unitcell::UnitCell{O}`
 * `name::O`
 """
-function hassite(uc::UnitCell{O}, name::O) where {O}
+function hassite(uc::UnitCell{S, O}, name::S) where {S, O}
     return haskey(uc.siteindices, name)
 end
 
@@ -225,7 +250,7 @@ Get index of the given site.
 * `unitcell::UnitCell{O}`
 * `name::O`
 """
-function getsiteindex(uc::UnitCell{O}, name::O) where {O}
+function getsiteindex(uc::UnitCell{S, O}, name::S) where {S, O}
     return uc.siteindices[name]
 end
 
@@ -242,7 +267,7 @@ Get the site (its site name and its fractional coordinates) with the given name.
 # Return
 * `(sitename, fractcoord)`
 """
-function getsite(uc::UnitCell{O}, name::O) where {O}
+function getsite(uc::UnitCell{S, O}, name::S) where {S, O}
     return uc.sites[uc.siteindices[name]]
 end
 
@@ -306,7 +331,7 @@ end
 # Return
 * `sitename`
 """
-function getsitename(uc::UnitCell{O}, index::Integer)::O where {O}
+function getsitename(uc::UnitCell, index::Integer)
     return uc.sites[index][1]
 end
 
@@ -323,6 +348,64 @@ end
 """
 function getsitecoord(uc::UnitCell, index::Integer)::FractCoord
     return uc.sites[index][2]
+end
+
+
+# === Orbitals ===
+
+function numorbitals(uc::UnitCell)
+    return length(uc.orbitals)
+end
+
+function numorbital(uc::UnitCell)
+    return length(uc.orbitals)
+end
+
+function orbitalcount(uc::UnitCell)
+    return length(uc.orbitals)
+end
+
+
+function addorbital!(uc::UnitCell{S, O}, orbitalname::O, siteindex::Integer) where {S, O}
+    1 <= siteindex <= sitecount(uc) || throw(ArgumentError("siteindex out of bounds"))
+    haskey(uc.orbitalindices, orbitalname) && throw(ArgumentError("duplicate orbital name"))
+    push!(uc.orbitals, (orbitalname, siteindex))
+    uc.orbitalindices[orbitalname] = length(uc.orbitals)
+    return length(uc.orbitals)
+end
+
+
+function hasorbital(uc::UnitCell{S, O}, name::O) where {S, O}
+    return haskey(uc.orbitalindices, name)
+end
+
+
+function getorbitalindex(uc::UnitCell{S, O}, name::O) where {S, O}
+    return uc.orbitalindices[name]
+end
+
+
+function getorbital(uc::UnitCell{S, O}, name::O) where {S, O}
+    return uc.orbitals[uc.orbitalindices[name]]
+end
+
+
+function getorbital(uc::UnitCell, index::Integer)
+    return uc.orbitals[index]
+end
+
+
+function getorbitalname(uc::UnitCell, index::Integer)
+    return uc.orbitals[index][1]
+end
+
+function getorbitalsiteindex(uc::UnitCell, index::Integer)
+    return uc.orbitals[index][2]
+end
+
+
+function getorbitalcoord(uc::UnitCell, index::Integer)
+    return getsitecoord(uc, getorbitalsiteindex(uc, index))
 end
 
 
